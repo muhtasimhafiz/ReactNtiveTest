13ed377f35d922779242215b2610c1a81a9db4ec
muhtasimhafiz <muhtasimhafiz@gmail.com>
2019-12-23 13:17:09 +0600

    first

---
+++ src/screens/emptyscreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ListScreen = () => {
+  return <Text></Text>;
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 45
+  },
+  subHeaderStyle: {}
+});
